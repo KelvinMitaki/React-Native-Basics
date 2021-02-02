3d166b65529301507bcec65b27fff5ba585b38fc
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-02 20:26:58 +0300

    added deleting item onpress

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Button, Input, ListItem } from "react-native-elements";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
@@ -18,7 +22,13 @@ export default function App() {
       <FlatList
         data={goals}
         keyExtractor={(_, i) => i.toString()}
-        renderItem={({ item }) => <GoalItem title={item} />}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            onPress={() => setGoals(g => g.filter((_, i) => index !== i))}
+          >
+            <GoalItem title={item} />
+          </TouchableOpacity>
+        )}
         style={{ marginHorizontal: 10 }}
       />
     </SafeAreaView>

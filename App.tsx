73d5bee6b02315d34dec182a83e7413d03342498
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-02 18:06:55 +0300

    added inp and btn

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button, Input } from "react-native-elements";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1
+      }}
+    >
+      <View style={styles.container}>
+        <Input placeholder="Course Goal" containerStyle={{ width: "70%" }} />
+        <Button title="ADD" containerStyle={{ width: "20%" }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 50
+  }
 });

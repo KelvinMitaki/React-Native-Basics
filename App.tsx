8cdb183c49e2ae78cdf839ee0878dfb7cad51c90
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-03 16:02:55 +0300

    added dismissing keyboard

--- App.tsx
+++ App.tsx
@@ -1,25 +1,35 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
-import { Button, Header } from "react-native-elements";
+import {
+  Keyboard,
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback
+} from "react-native";
+import { Header } from "react-native-elements";
 import StartGameScreen from "./screens/StartGameScreen";
 // import Header from "./components/Header";
 
 const App = () => {
   return (
-    <SafeAreaView>
-      {/* <Header /> */}
-      <Header
-        placement="left"
-        leftComponent={{ icon: "menu", color: "#fff" }}
-        centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
-        rightComponent={{ icon: "home", color: "#fff" }}
-        containerStyle={{ marginTop: 20 }}
-      />
-      <StartGameScreen />
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.view}>
+        <Header
+          placement="left"
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+          containerStyle={{ marginTop: 20 }}
+        />
+        <StartGameScreen />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view: {
+    flex: 1
+  }
+});
